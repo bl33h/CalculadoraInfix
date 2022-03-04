31a382a82e22057a53a06d4c46ea3fdf3bfb1a6e
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-03-04 13:18:17 -0600

    javadoc & VectorStack.

--- ListStack.java
+++ ListStack.java
@@ -8,8 +8,12 @@ public class ListStack <T> extends Stack<T>{
     - Última modificación:
     */
     
-    private List <T> list = null;
+    private List <T> list = null; // objeto
 
+    /*****************************************************************
+     * instancia la opcion de lista.
+     * @param listOption
+     */
     public ListStack(int listOption) {
         super();
 		if (listOption == 1)
@@ -17,32 +21,58 @@ public class ListStack <T> extends Stack<T>{
         else if (listOption == 2)
             list = new DoubleLinkedList<T>();
 	}
+    //****************************************************************
 
+
+    /*****************************************************************
+     * inserta un valor al final.
+     * @param value
+     */
     @Override
     public void push(T value) {
         list.InsertAtEnd(value);
     }
+    //****************************************************************
 
+    /*****************************************************************
+     * retorna el último valor ingresado y lo elimina de la pila.
+     * @return T
+     */
     @Override
     public T pull() {
         return list.DeleteAtEnd();
     }
+    //****************************************************************
 
+    /*****************************************************************
+     * retorna el último valor de la pila.
+     * @return T
+     */
     @Override
     public T peek() {
         return list.Get(count()-1);
     }
+    //****************************************************************
 
-
+    /*****************************************************************
+     * retorna el número de elementos en la pila.
+     * @return int
+     */
     @Override
     public int count() {
         return list.Count();
     }
+    //****************************************************************
 
+    /*****************************************************************
+     * retorna el estado de la pila.
+     * @return boolean
+     */
     @Override
 	public boolean isEmpty() {		
 		return list.IsEmpty();
 	}
+    //****************************************************************
 
 }
 
--- VectorStack.java
+++ VectorStack.java
@@ -1,4 +1,5 @@
-public class VectorStack <T> {
+import java.util.Vector;
+public class VectorStack <T> extends Stack <T>{
     /**
     * Copyright (C), 2022-2023, FabianJuarez SaraEcheverria Jose Pablo Kiesling
     * @author Fabian Juarez, Sara Echeverria y Jose Pablo Kiesling
@@ -6,9 +7,73 @@ public class VectorStack <T> {
     @version:
     - Creación: 04/03/2022
     - Última modificación:
-    Clase que sirve como vista hacia el usuario la cual imprimira a pantalla
     */
 
-    private VectorStack <T> vector;
+    private Vector<T> vector;
 
+    /*****************************************************************
+     * se instancia el vector.
+     * @param 
+     */
+    public VectorStack (){
+        vector = new Vector<T>();
+    }
+    //****************************************************************
+
+    /*****************************************************************
+     * push: agrega un elemento genérico.
+     * @param value 
+     */
+    @Override
+    public void push(T value) {
+        vector.add(value);
+    }
+    //****************************************************************
+
+    /*****************************************************************
+     * pull: retorna el último valor ingresado y lo elimina de la pila.
+     * @return T
+     */
+    @Override
+    public T pull() {
+        T value;
+        if (isEmpty()) //Si la pila está vacía
+            value = null;
+        else{ //Si se tiene algún elemento
+            value = peek();
+            vector.remove(vector.get(count()-1));
+        }
+        return value;
+    }
+    //****************************************************************
+
+    /*****************************************************************
+     * peek: retorna el último valor de la pila.
+     * @return T
+     */
+    @Override
+    public T peek() {
+        return vector.lastElement();
+    }
+    //****************************************************************
+
+    /*****************************************************************
+     * count: retorna el número de elementos en la pila.
+     * @return int
+     */
+    @Override
+    public int count() {
+        return vector.size();
+    }
+    //****************************************************************
+
+    /*****************************************************************
+     * isEmpty: retorna el estado de la pila.
+     * @return boolean
+     */
+    @Override
+    public boolean isEmpty() {
+        return vector.isEmpty();
+    }
+    //****************************************************************
 }
