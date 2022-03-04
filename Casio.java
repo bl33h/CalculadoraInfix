0ba8de10db499620c7b29988de5db46fd681415d
FabianJuarez182 <jua21440@uvg.edu.gt>
2022-03-04 09:59:51 -0600

    creacion de Casio con su paso de expresiones y lectura de datos, completada

--- Casio.java
+++ Casio.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Casio {
     /**
     * Copyright (C), 2022-2023, FabianJuarez SaraEcheverria Jose Pablo Kiesling
@@ -6,6 +10,48 @@ public class Casio {
     @version:
     - Creación: 04/03/2022
     - Última modificación:
-    Clase que sirve como vista hacia el usuario la cual imprimira a pantalla
+    Clase que sirve como paso de expresiones y lectura del archivo
     */
+    private Calculator calculator = new Calculator();
+    private Stack<String> operations = new Stack<String>();
+    //---------------------------METODOS------------------------------
+    /*****************************************************************
+	 * FileReader: Lee el archivo.
+     * @return boolean
+     * @throws FileNotFoundException
+     */
+    private boolean read() throws FileNotFoundException{
+        boolean read = false;
+        try{
+            read = true;
+            File file = new File("datos.txt");
+            Scanner reader = new Scanner(file);
+            while(reader.hasNextLine()){
+                operations.push(reader.nextLine());
+                reader.close();
+            }
+        }catch(FileNotFoundException e){
+            String s = "Casio.read:" + e.getMessage();
+            throw new FileNotFoundException(s);
+        }
+        return read;
+    }
+    //****************************************************************
+
+    /*****************************************************************
+    /**
+     * doOperation: hace operaciones cargadas en la pila
+     * @return String
+     * @throws FileNotFoundException
+     */
+    public void doOperation() throws FileNotFoundException{
+
+        if(read())
+            while(!this.operations.isEmpty()){
+                String operation = this.operations.pull();
+                int result = calculator.Evaluate(operation);
+                System.out.println(operation + "\n" + result + "\n"+ "\n");
+            }
+    }
+    //****************************************************************
 }
