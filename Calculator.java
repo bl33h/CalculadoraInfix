d0ee856ff9811e8290c27084e01962da33c0595b
TheKiesling <kie21581@uvg.edu.gt>
2022-03-04 10:44:53 -0600

    Create Calculator.java
    Class that operates an expresion

---
+++ Calculator.java
@@ -0,0 +1,104 @@
+/*
+#      #    #######    ########   #######   #          #######   ##      #    #########
+#     #        #       #          #         #             #      # #     #    #
+#    #         #       #          #         #             #      #  #    #    #
+####           #       #####      #######   #             #      #   #   #    #    ####
+#    #         #       #                #   #             #      #    #  #    #       #
+#     #        #       #                #   #             #      #     # #    #       #
+#      #    ########   ########   #######   ########   #######   #      ##    #########
+*/
+
+import java.util.Arrays;
+
+public class Calculator {
+    /**
+    * Copyright (C), 2022-2023, The_Kiesling FabianJuarez SaraEcheverria 
+    * @author Jose Pablo Kiesling, Fabian Juarez y Sara Echeverria
+    Nombre del programa: InfixPostfix.java
+    @version: 
+        - Creación: 04/03/2022
+        - Última modificación: 
+
+    Clase que calcula la expresion
+    */
+    //---------------------------PROPIEDADES--------------------------
+    private Stack<Integer> stack;
+    private InfixPostfix converter = new InfixPostfix();
+    private String[] operands = {"0", "1", "2","3","4","5","6","7","8","9"};
+    private String[] operators = {"+","-","*","/"};
+
+    /*****************************************************************
+	 * Evaluate: evalúa la expresión ingresada y retorna el resultado
+	 * @param expresion
+	 * @return int
+     * @Override
+	 */
+    public int Evaluate(String expresion){
+        String postfixExpresion = converter.convert(expresion);
+        stack = new Stack<Integer>();
+        boolean flag = true;
+        int num1, num2, result = 0;
+        String[] values = postfixExpresion.split(" "); //Separar los valores en la expresión
+        
+        for(int i = 0; i < values.length && flag; i++){ //Recorrer toda la expresión
+                
+            if(Arrays.asList(operands).contains(values[i])) //Si es Número
+                stack.push(Integer.parseInt(values[i]));
+
+            else if(Arrays.asList(operators).contains(values[i])){ //Si es Operador
+                if (stack.count() >= 2){ //Si hay dos o más en la pila es porque se pueden hacer operaciones
+
+                    //Sacar los dos últimos números ingresados
+                    num2 = stack.pull();
+                    num1 = stack.pull();
+                    switch(values[i]){
+                        case "+": //Suma
+                            stack.push(num1 + num2);
+                        break;
+
+                        case "-": //Resta
+                            stack.push(num1 - num2);
+                        break;
+
+                        case "*": //Multiplicación
+                            stack.push(num1 * num2);
+                        break;
+
+                        case "/": //División
+                            if (num2 != 0) //Si el segundo número es distinto de cero se puede hacer la división
+                                stack.push(num1 / num2);
+                            else{ //División indefinida
+                                stack.push(9999);
+                                System.out.println("¡ERROR! División entre cero!");
+                                flag = false;
+                            }
+                        break;
+
+                    }
+                }
+                else{ //Faltan números
+                    System.out.println("¡ERROR! Le han faltado operandos!");
+                    flag = false;
+                }
+            }
+            else{ //No ingresó números ni operadores
+                stack.push(9999);
+                System.out.println("¡ERROR! Ha ingresado un carácter inválido!");
+                flag = false;
+            }
+        }
+        if (stack.count() == 1 && flag) //Si ya solo queda un elemento en la pila
+            result = stack.pull();
+        else if (!flag) //Si queda más de un elemento es porque faltaron operadores
+            stack.push(9999);
+        else {
+            System.out.println("¡ERROR! Le han faltado operadores!");
+            flag = false;
+        }
+        if (flag) //Si la operación es válida
+            return result;
+        else //Existió algún error respecto a la operación (sintáxis o lógica)
+            return 9999;
+    }
+    //****************************************************************
+}
