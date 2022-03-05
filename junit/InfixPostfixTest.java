0141446d8700aa3934cac87ed9ea5c32c8a69583
FabianJuarez182 <jua21440@uvg.edu.gt>
2022-03-04 22:07:56 -0600

    Arreglo de junitpostfixtest e imagen de la comprobacion de la prueba

--- junit/InfixPostfixTest.java
+++ junit/InfixPostfixTest.java
@@ -9,9 +9,10 @@ public class InfixPostfixTest {
 
 	@Test
     public void InfixCalculator(){
-        Calculator calculator = new Calculator();
-        assertEquals(55, calculator.Evaluate("1 2 + 4 * 1 - 5 *"));
-        assertEquals(-1, calculator.Evaluate("4 5 -"));
-        assertEquals(-7, calculator.Evaluate("9 6 5 * - 3 /"));
-    }
+    Calculator calculator = new Calculator();
+    Stack<Integer> stackInteger = (new FactoryCasio<Integer>()).newCasio(1, 1);
+    Stack<String> stackString = (new FactoryCasio<String>()).newCasio(1,1);
+    assertEquals(55, calculator.Evaluate("( ( ( ( 1 + 2 ) * 4 ) - 1 ) * 5 ) ", stackInteger, stackString, 1));
+    assertEquals(1, calculator.Evaluate("( 5 - 4 )",stackInteger, stackString, 1));
+    //
 }
